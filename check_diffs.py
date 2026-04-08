6b0c16adef5133089cb7edbf46496d1c4b8e2767
Sabrina <se1854@princeton.edu>
2026-04-08 13:20:03 -0400

    comments

--- check_diffs.py
+++ check_diffs.py
@@ -4,10 +4,15 @@ import pandas as pd
 df = pd.read_csv("outputs_ood/olmo-3-7b/logits_test_results_swap3_1000_0.csv")
 filtered = df[df["p_NE_a"] != df["p_BR_to_opp"]]
 
+# num rows that are 0 or 1 for each
 print(sum(df['p_NE_a'] == 0))
+print(sum(df['p_NE_a'] == 1))
 print(sum(df['p_BR_to_opp'] == 0))
+print(sum(df['p_BR_to_opp'] == 1))
 
-# print(sum(df['p_NE_a'] == df['p_BR_to_opp']))
-# print(sum(df['p_NE_a'] != df['p_BR_to_opp']))
+# num rows same, num rows diff
+print(sum(df['p_NE_a'] == df['p_BR_to_opp']))
+print(sum(df['p_NE_a'] != df['p_BR_to_opp']))
 
-# print(sum(df['p_NE_a'] == df['p_BR_to_opp']) / len(df))
+# percent of rows that are same
+print(sum(df['p_NE_a'] == df['p_BR_to_opp']) / len(df))
